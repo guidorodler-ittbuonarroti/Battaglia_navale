a52802caa1c80d7a7028c96846d087fb7800880b
GUIDO.RODLER <GUIDO.RODLER@buonarroti.tn.it>
2023-05-04 10:06:55 +0200

    feat:added some GUI classes

--- ProgettoIntellij/src/start.java
+++ ProgettoIntellij/src/start.java
@@ -1,2 +1,30 @@
-public class start {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class start extends JFrame{
+    private JButton btnInizia;
+    private JPanel pnlPannello;
+    private JLabel lblBenvenuto;
+
+    start(){
+        setContentPane(pnlPannello);
+        setTitle("Giuco pazzo sgravo");
+        setSize(950,400);
+        setLocation(600,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        btnInizia.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+    public static void main(String[] args){
+        start ciao = new start();
+
+    }
+
 }
